ad1b96e9b84728133c4cbc89d73c05fb95c83b87
Thomas Calle <thos.calle@gmail.com>
2023-03-07 23:11:00 -0500

    Thomas & Friends

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,39 +1,38 @@
 INSERT INTO departments (department_name)
 VALUES 
-('Sales'),
+('Executive Board'),
 ('Marketing'),
 ('Human Resources'),
 ('Finance'),
 ('Engineering'),
 ('Information Technology'),
-('Customer Service'),
+('Customer Relations'),
 ('Research and Development'),
 ('Legal'),
-('Operations');
+('Maintenance');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES 
-('Manager', 85000.00, 1),
-('Assistant Manager', 55000.00, 1),
-('Sales Associate', 129000.00, 1),
-('Marketing Manager', 90000.00, 2),
-('Marketing Coordinator', 75000.00, 2),
-('HR Manager', 120000.00, 3),
-('HR Coordinator', 85000.00, 3),
-('Finance Manager', 95000.00, 4),
-('Senior Engineer', 180000.00, 5),
-('Software Developer', 85000.00, 5);
+('Chief Executive Officer', 555000.00, 1),
+('Marketing Manager', 125000.00, 2),
+('HR Director', 189000.00, 3),
+('Finance Head', 145000.00, 4),
+('Senior Engineer', 185000.00, 5),
+('IT Manager', 125000.00, 6),
+('Customer Relations Manager', 75000.00, 7),
+('Research and Development Manager ', 185000.00, 8),
+('Legal Manager', 95000.00, 9),
+('Maintenance Manager', 135000.00, 10);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('Tom', 'Brady', 1, 1),
-('Thomas', 'The Tank Engine', 2, 1),
-('Tom', 'Hanks', 3, 1),
-('Thomas', 'Hobbes', 4, 1),
-('Tom', 'Cruise', 5, 1),
-('Thomas', 'Shelby', 6, 1),
-('Tom', 'Ford', 7, 1),
-('Thomas', 'Calle', 5, 1),
-('Tom', 'Holland', 9, 1),
-('Thomas', 'The Apostle', 10, 1);
-
+('Thomas', 'The Tank Engine', 2, 2),
+('Thomas', 'Calle', 3, 3),
+('Thomas', 'Shelby', 4, 4),
+('Thomas', 'Hobbes', 5, 5),
+('Tom', 'Hanks', 6, 6),
+('Tom', 'Holland', 7, 7),
+('Tom', 'Ford', 8, 8),
+('Thomas', 'The Apostle', 9, 9)
+('Tom', 'Cruise', 10, 10),;
